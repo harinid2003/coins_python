6871e6b3803a9016f1a3897e15f137ab10afeef6
HARINI D <106166069+harinid2003@users.noreply.github.com>
2024-05-27 10:57:38 +0530

    Update random_names.py

--- random_names.py
+++ random_names.py
@@ -3,6 +3,7 @@ import random
 user = input("Enter names seperated by(,): ")
 list = user.split(',')
 print(list)
-list1 = random.randint(0,1)
+length = len(list)
+list1 = random.randint(0,length-1)
 print(list1)
 print(list[list1])
